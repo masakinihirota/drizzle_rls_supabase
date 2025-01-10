f745806f1223bbc35a13990740d33904a2ea7572
masakinihirota <masakinihirota@gmail.com>
2025-01-10 10:23:50 +0900

    ログインページのHonoリンクを整理し、不要な改行を削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,8 @@ export default function Home() {
       {/* ログインページへのリンク */}
       <a href="/login" className="text-blue-200">ログイン</a>
       <br />
-      <br />
       {/* Honoページへのリンク */}
-      <a href="/hono" className="text-blue-200">Hono</a>
-      <br />
-
+      <a href="/hono">Hono</a>
     </div>
   );
 }
